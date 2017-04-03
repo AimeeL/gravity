be59e1f74f7e5b1ba902137a1fb8a8be5c87f5c6
Aimee <Aimee@DESKTOP-06A6TF6>
2017-04-03 16:45:46 -0300

    refactoring step 3

--- src/main/java/dal/gravity/SimplePendulum.java
+++ src/main/java/dal/gravity/SimplePendulum.java
@@ -3,7 +3,7 @@ package dal.gravity;
 /**
  * Represents pendulums exhibiting (approximately) simple harmonic motion
  */
-public class SimplePendulum extends AbstractEarthPendulum {
+public class SimplePendulum extends AbstractPendulum {
 
     private double angularFrequency, periodOfMotion;
 
@@ -13,10 +13,10 @@ public class SimplePendulum extends AbstractEarthPendulum {
      * inMass: the point mass (>0)
      * inTheta0: angular displacement at t=0 (0<=theta0<=pi/6)
      */
-    public SimplePendulum (double inLength, double inMass, double inTheta0) {
-	super (inLength, inMass, inTheta0);
-	angularFrequency = Math.sqrt (this.getGravitationalField () / this.getStringLength ());
-	periodOfMotion = 2 * Math.PI 
+    public SimplePendulum (double inLength, double inMass, double inTheta0, GravityModel g) {
+    	super (inLength, inMass, inTheta0, g);
+    	angularFrequency = Math.sqrt (this.getGravitationalField () / this.getStringLength ());
+    	periodOfMotion = 2 * Math.PI 
 	    * Math.sqrt (this.getStringLength () / this.getGravitationalField ());
     }
 
@@ -34,6 +34,6 @@ public class SimplePendulum extends AbstractEarthPendulum {
      * provides the angular displacement of this Pendulum at time t
      */
     public double getTheta (double t) {
-	return this.getMaxAngularDisplacement () * Math.cos (angularFrequency * t);
+    	return this.getMaxAngularDisplacement () * Math.cos (angularFrequency * t);
     }
 }
